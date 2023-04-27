caa6cb0dda6cb7b583087e5804e89efc9da0b4bf
HighPriestCode <71380116+neos53xbox@users.noreply.github.com>
2023-04-27 12:40:08 +0200

    Update http_logger.py

--- Jorge/http_logger.py
+++ Jorge/http_logger.py
@@ -17,7 +17,8 @@ block_duration = 300
 blocked_ips = {}
 
 # Expression régulière pour détecter les injections SQL
-sql_injection_pattern = re.compile(r"('|\"|;|--|\b(?:SELECT|INSERT|UPDATE|DELETE|FROM|WHERE)\b)", re.IGNORECASE)
+# sql_injection_pattern = re.compile(r"('|\"|;|--|\b(?:SELECT|INSERT|UPDATE|DELETE|FROM|WHERE)\b)", re.IGNORECASE)
+sql_injection_pattern = re.compile(\s*([\0\b\'\"\n\r\t\%\_\\]*\s*(((select\s*.+\s*from\s*.+)|(insert\s*.+\s*into\s*.+)|(update\s*.+\s*set\s*.+)|(delete\s*.+\s*from\s*.+)|(drop\s*.+)|(truncate\s*.+)|(alter\s*.+)|(exec\s*.+)|(\s*(all|any|not|and|between|in|like|or|some|contains|containsall|containskey)\s*.+[\=\>\<=\!\~]+.+)|(let\s+.+[\=]\s*.*)|(begin\s*.*\s*end)|(\s*[\/\*]+\s*.*\s*[\*\/]+)|(\s*(\-\-)\s*.*\s+)|(\s*(contains|containsall|containskey)\s+.*)))(\s*[\;]\s*)*)+)", re.IGNORECASE)
 
 # Durée de blocage en secondes
 block_duration_ip = 300
